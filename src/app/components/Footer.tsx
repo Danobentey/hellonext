0ec917336339aaf0a657f13a401c38187e0ea1f1
Paul-Chukwuemeka <paulchukwuemeka123@gmail.com>
2024-08-26 15:04:54 +0100

    updated design

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -42,13 +42,13 @@ const Footer = () => {
   ];
 
   return (
-    <div className="mt-[-59px] w-[101%] ml-[-10px] p-[20px] rounded-tr-[15px] rounded-tl-[15px] h-[300px]flex flex-col gap-4 bg-[#19200e] sm:flex-row sm:justify-between">
-      <div className="">
+    <div className="mt-[-59px] w-[101%] ml-[-10px] p-[20px] rounded-tr-[15px] rounded-tl-[15px] h-[300px]flex flex-col gap-4 sm:flex-row sm:justify-between">
+      <div className="ml-12 mt-12">
         <img src="" alt="" />
-        <Button title="Join the Waiting List" />
+        <Button  title="Join the Waiting List" />
       </div>
 
-      <div className="flex gap-4 justify-between sm:justify-normal mt-[-40px] border ml-[55%] pr-12 pt-12 pb-12">
+      <div className="flex gap-4 justify-between sm:justify-normal mt-[-70px] ml-[50%] pr-12 pt-6 pb-12">
         <nav className="ml-[6%] flex flex-col">
           {navigation.map((item) => (
             <a
@@ -58,7 +58,7 @@ const Footer = () => {
               className={classNames(
                 item.current()
                   ? " underline p-50 text-white"
-                  : "text-[35px] p-[20px] text-gray-300 hover:bg-gray-700 hover:text-white",
+                  : "text-[40px] p-[20px] text-gray-300 hover:bg-gray-700 hover:text-white",
                 "rounded-md text-sm font-medium"
               )}
               onClick={() => setActive(item.name)}
@@ -68,16 +68,17 @@ const Footer = () => {
           ))}
         </nav>
 
-        <div className="flex flex-col gap-4">
-          <p className="flex text-[25px] place-items-center gap-1">
-            <img src={ICONS.linkedin.src} alt="linkedin" className="h-[30px]" />
+        <div className="flex flex-col gap-4 mt-[20px]">
+          <p className="text-white text-[18px]">Contact us </p>
+          <p className="flex text-[15px] text-white place-items-center gap-1">
+            <img src={ICONS.linkedin.src} alt="linkedin" className="h-[20px]" />
             LinkedIn
           </p>
-          <p className="flex text-[25px] place-items-center gap-1">
-            <img src={ICONS.fb.src} alt="fb" className="h-[30px]" />
+          <p className="flex text-[15px] text-white place-items-center gap-1">
+            <img src={ICONS.fb.src} alt="fb" className="h-[20px]" />
             Facebook
           </p>
-          <p className="flex text-[25px] place-items-center gap-1">
+          <p className="flex text-[15px] text-white place-items-center gap-1">
             <img src={ICONS.insta.src} alt="insta" className="h-[30px]" />
             Instagram
           </p>
